bbd1f32920e26394a3bad3d533fe27b537a4be9d
Dwang-ML <0XXXXX.ML.XXXXX0@gmail.com>
2025-08-17 08:34:31 -0600

    Setup for SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://skyfold.dev',
+  generateRobotsTxt: true,
+  outDir: './public', // ensures sitemap & robots.txt end up in public/
+}
